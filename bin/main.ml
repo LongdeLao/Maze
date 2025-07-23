2dc4dcfb42f838251a5f30a845f46fda3a5c06a3
Leo <125950588+LongdeLao@users.noreply.github.com>
2025-07-23 17:42:57 +0800

    added tests

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@ open GMain
 
 let () =
   let _ = GMain.init () in
-  let window = GWindow.window ~title:"Simple window" ~border_width:10 () in
+  let window = GWindow.window ~title:"Simple window" ~width:800 ~height:800 ~border_width:10 () in
   ignore (window#connect#destroy ~callback:GMain.quit);
   window#show ();
   GMain.main ()
