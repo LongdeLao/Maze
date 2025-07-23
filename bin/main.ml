1ce8a9616a6ebe25f877a659c3b86bb701cee879
Leo <125950588+LongdeLao@users.noreply.github.com>
2025-07-23 17:15:13 +0800

    edited dependencies and made window available

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,8 @@
-let () = print_endline "Hello, World!"
-(*test*)
+open GMain
+
+let () =
+  let _ = GMain.init () in
+  let window = GWindow.window ~title:"Maze" ~border_width:10 () in
+  ignore (window#connect#destroy ~callback:GMain.quit);
+  window#show ();
+  GMain.main ()
